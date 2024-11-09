3a0c6934c2dd84a0df878fc926f4b0db2adeb7ab
Acosta <cristian.acosta01@icloud.com>
2024-11-09 15:56:10 -0300

    test

--- src/main/java/models/Producto.java
+++ src/main/java/models/Producto.java
@@ -3,7 +3,7 @@ package models;
 public class Producto {
     private int id;
     private String nombre;
-    private String descripcion; // otra prueba
+    private String descripcion; // otra prueba // Hola
     private double precio;
     private String imagen;  
 
